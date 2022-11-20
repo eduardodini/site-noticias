53a75ce555db2fc32ba9b27d5c6f8df0145733f1
Eduardo <eduardodiasdini@gmail.com>
2022-11-20 16:44:30 -0300

    Add footer

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './App.css';
 import Navbar from './components/navbar/index';
+import Footer from './components/footer/footer'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Main from './pages';
 import Politics from './pages/politics';
@@ -8,18 +9,22 @@ import Education from './pages/health';
 import Health from './pages/health';
 import Contact from './pages/contact';
 
+
 function App() {
   return (
-    <Router>
-      <Navbar />
-      <Routes>
-        <Route path='/' exact component={Main} />
-        <Route path='/about' component={Politics} />
-        <Route path='/events' component={Education} />
-        <Route path='/annual' component={Health} />
-        <Route path='/team' component={Contact} />
-      </Routes>
-    </Router>
+    <><>
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path='/' exact component={Main} />
+          <Route path='/about' component={Politics} />
+          <Route path='/events' component={Education} />
+          <Route path='/annual' component={Health} />
+          <Route path='/team' component={Contact} />
+        </Routes>
+      </Router>
+    </><Footer /></>
+
   );
 }
 
