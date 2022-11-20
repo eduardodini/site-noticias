f291ed12f1e1fa8600fce7f851978819887b4bbc
Eduardo <eduardodiasdini@gmail.com>
2022-11-20 19:02:25 -0300

    Ugly contact form

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,22 +1,46 @@
 import React from 'react';
-import NewsBox from '../components/newsbox/newsbox';
 
 const Contact = () => {
+    const [formStatus, setFormStatus] = React.useState('Send')
+    const onSubmit = (e) => {
+      e.preventDefault()
+      setFormStatus('Submitting...')
+      const { name, email, message } = e.target.elements
+      let conFom = {
+        name: name.value,
+        email: email.value,
+        message: message.value,
+      }
+      console.log(conFom)
+    }
     return (
-        <div
-            style={{
-                display: 'flex',
-                justifyContent: 'Right',
-                alignItems: 'Right',
-                height: '100vh'
-            }}
-        >
-            <NewsBox />
-            <NewsBox />
-            <NewsBox />
-            <NewsBox />
-        </div>
-    );
-};
+      <div className="container mt-5">
+        <h2 className="mb-3">React Contact Form Component Example</h2>
+        <form onSubmit={onSubmit}>
+          <div className="mb-3">
+            <label className="form-label" htmlFor="name">
+              Name
+            </label>
+            <input className="form-control" type="text" id="name" required />
+          </div>
+          <div className="mb-3">
+            <label className="form-label" htmlFor="email">
+              Email
+            </label>
+            <input className="form-control" type="email" id="email" required />
+          </div>
+          <div className="mb-3">
+            <label className="form-label" htmlFor="message">
+              Message
+            </label>
+            <textarea className="form-control" id="message" required />
+          </div>
+          <button className="btn btn-danger" type="submit">
+            {formStatus}
+          </button>
+        </form>
+      </div>
+    )
+  }
 
 export default Contact;
