2f216cb292d0c0430b439046bff522fd1f5aaad5
Eduardo <eduardodiasdini@gmail.com>
2022-12-18 14:34:51 -0300

    Add searchbar to navbar

--- src/components/navbar/index.js
+++ src/components/navbar/index.js
@@ -5,6 +5,8 @@ import {
     Bars,
     NavMenu
 } from './NavbarElements'
+import Form from 'react-bootstrap/Form'
+import Button from 'react-bootstrap/Button'
 
 const Navbar = () => {
     return (
@@ -17,6 +19,15 @@ const Navbar = () => {
                 <NavLink to='/education'>Educação </NavLink>
                 <NavLink to='/health'>Saúde</NavLink>
                 <NavLink to='/contact'>Contato</NavLink>
+                <Form className="d-flex">
+                    <Form.Control
+                        type="search"
+                        placeholder="Search"
+                        className="me-2"
+                        aria-label="Search"
+                    />
+                    <Button variant="outline-success">Pesquisar</Button>
+                </Form>
             </NavMenu>
         </Nav>
         </>
