f0f920d3391be82f8e1e9feda3eab1ed77cc443f
Eduardo <eduardodiasdini@gmail.com>
2022-12-18 18:10:05 -0300

    Firebase hosting + like button

--- src/components/newsbox/newscard.js
+++ src/components/newsbox/newscard.js
@@ -1,12 +1,7 @@
 import React from 'react';
 import { Card, Button } from 'react-bootstrap';
-import { Link } from 'react-router-dom';
 
 class NewsCard extends React.Component {
-    constructor(props) {
-        super(props);
-    }
-
     componentDidMount() {
     }
 
@@ -17,7 +12,8 @@ class NewsCard extends React.Component {
                 <Card.Body>
                     <Card.Title>{this.props.article.title ? this.props.article.title.substr(0, 120) : ""}</Card.Title>
                     <Card.Text>{this.props.article.description ? this.props.article.description.substr(0, 480) : ""}</Card.Text>
-                    <Button variant="primary">Leia tudo</Button>
+                    <Button variant="primary" padding="5px">Leia tudo</Button>
+                    <Button variant="secondary">Curtir 👍</Button>
                 </Card.Body>
             </Card>
         )
